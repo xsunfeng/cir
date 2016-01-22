c98686f3437318d2862889e3ec427dfd2244be2c
Feng Sun <xsunfeng@gmail.com>
2016-01-22 12:32:13 -0500

    deduplicate link thickness

--- cir/sankey_views.py
+++ cir/sankey_views.py
@@ -8,21 +8,21 @@ def get_graph(request):
 	graph["links"] = []
 	forum_id = request.session['forum_id']
 	count = 0
-
+	# create nodes for not-alone themes
 	themes = ClaimTheme.objects.filter(forum_id=request.session['forum_id'])
 	for theme in themes:
-		graph["nodes"].append({"node": count, "name": str(theme),  "more": "theme-" + str(theme.id) })
-		count = count + 1
+		if (Highlight.objects.filter(theme_id = theme.id).count() != 0):
+			graph["nodes"].append({"node": count, "name": str(theme),  "more": "theme-" + str(theme.id) })
+			count = count + 1
+	# create nodes for not-alone sections
 	sections = DocSection.objects.filter(forum_id=request.session['forum_id'])
 	for section in sections:
 		if (Highlight.objects.filter(context_id = section.id).count() != 0):
-			print section.id
-			print Highlight.objects.filter(context_id = section.id)[0].id
-			print "====="
-			graph["nodes"].append({"node": count, "name": "section" + str(section.id), "more": "section-" + str(section.id) })
+			graph["nodes"].append({"node": count, "name": section.title, "more": "section-" + str(section.id) })
 			count = count + 1
-	print graph["nodes"]
+	# create links among themes and sections
 	highlights = Highlight.objects.all()
+	set = []
 	for h in highlights:
 		if (h.context != None and h.theme != None):
 			sec_id = h.context.id
@@ -30,15 +30,17 @@ def get_graph(request):
 			source_node = get_item_by_more(graph["nodes"], "section-" + str(sec_id))
 			target_node = get_item_by_more(graph["nodes"], "theme-" + str(theme_id))
 			if (source_node != None and target_node != None):
-				if ({"source": source_node["node"], "target": target_node["node"], "value":1} not in graph["links"]):
+				key = str(source_node["node"]) + " " + str(target_node["node"])
+				if key not in set:
+					set.append(key)
 					graph["links"].append({"source": source_node["node"], "target": target_node["node"], "value":1})
 				else:
 					for item in graph["links"]:
 						if (item["source"] == source_node["node"] and item["target"] == target_node["node"]):
 							item["value"] = item["value"] + 1
 							break
-	for item in graph["nodes"]:
-		print item
+	# for item in graph["links"]:
+	# 	print item
 	# for item in graph["links"]:
 	# 	print item
 	# graph["nodes"].append({'node': 0, 'name': 'theme0', 'more': 'theme-23'})
