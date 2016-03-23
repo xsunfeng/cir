5c87484ff5b11426234a3624356779bd3a0fe6ad
wafield <wafield.pku@gmail.com>
2016-03-23 09:35:10 -0400

    hardcode socket io address

--- static/js/layout/sankey_layout.js
+++ static/js/layout/sankey_layout.js
@@ -34,7 +34,7 @@ define([
     module.barchart_ajax;
     module.nodeMap = {};
     module.time_bound_list = [];
-    module.focus_threshold = 24;
+    module.focus_threshold = 300;
 
     module["relation"] = $("#sankey-relation .item.active").attr("data-value");
 
